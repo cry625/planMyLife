689279b6bf73691f46d2777f05c02b221d787542
caory <13350165610@163.com>
2025-01-20 16:59:07 +0800

    250120

--- planMyLife_front/src/components/BubbleBox.vue
+++ planMyLife_front/src/components/BubbleBox.vue
@@ -1,25 +1,29 @@
 <template>
-<div class="bubble-box-container">
-  <div class="extend-box">
-    <el-button v-if="!props.isExpand" :icon="ArrowDownBold" type="primary" plain round @click="expandAllNodes"></el-button>
-    <el-button v-else :icon="ArrowUpBold" type="warning" plain round @click="collapseAllNodes"></el-button>
+  <div class="bubble-box-container">
+    <div class="extend-box">
+      <el-button v-if="!props.isExpand" :icon="ArrowDownBold" type="primary" plain round
+        @click="expandAllNodes"></el-button>
+      <el-button v-else :icon="ArrowUpBold" type="warning" plain round @click="collapseAllNodes"></el-button>
+    </div>
+    <div class="add-btn">
+      <el-button :icon="CirclePlusFilled" type="success" plain round @click="addNewNote"></el-button>
+    </div>
+    <DialogNode :isShow="addDialogIsShow" module="add" Node="{}"/>
   </div>
-  <div class="add-btn">
-    <el-button :icon="CirclePlusFilled" type="success" plain round></el-button>
-  </div>
-</div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import DialogNode from './DialogNode.vue';
 import {
   ArrowDownBold,
   ArrowUpBold,
   CirclePlusFilled,
 } from '@element-plus/icons-vue'
+const addDialogIsShow = ref(false)
 // 接收父组件传递的树状结构存储对象
 const props = defineProps({
-  isExpand:false,
+  isExpand: false,
   treeStore: {
     type: Object,
     required: true
@@ -37,28 +41,33 @@ const collapseAllNodes = () => {
   props.treeStore.collapseAll();
   console.log('收缩所有节点');
 };
+function addNewNote() {
+  addDialogIsShow.value = true
+}
 </script>
 
 <style scoped>
 .bubble-box-container {
-  position: fixed; 
-  bottom: 10%; 
+  position: fixed;
+  bottom: 10%;
   right: 10%;
   display: flex;
-  flex-direction: column; 
-  align-items: center; 
-  justify-content: center; 
-  background-color: #fff; 
-  border: 1px solid #ddd; 
-  border-radius: 20px; 
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 20px;
   padding: 10px;
   z-index: 99;
 }
+
 .extend-box {
   display: flex;
   justify-content: space-around;
 }
-.add-btn{
+
+.add-btn {
   margin-top: 10px;
 }
 </style>
---
+++ planMyLife_front/src/components/DialogNode.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-dialog v-model="props.isShow" :title="props.module" width="50%">
+      <p>Add a new note here.</p>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="handleCancel">取消</el-button>
+          <el-button type="primary" @click="props.confirmFunc">
+            确定
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
+</template>
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  isShow: Boolean,
+  module:String,//'add', 'check', 'update'
+  Node:Object,
+  confirmFunc:Function,
+  // cancelFunc:Function
+})
+const emit = defineEmits(['update:props.isShow'])
+// 处理取消按钮的点击事件
+const handleCancel = () => {
+  emit('update:props.isShow', false) // 发出事件，更新父组件中的 isShow
+}
+
+</script>
+<style lang="less" scoped>
+
+</style>
